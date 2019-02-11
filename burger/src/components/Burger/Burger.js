17afcacad1dcef1073a13d3ced71cc35046b7fef
tal braun <talbraun@h-MBP-sl-tal.lan>
2019-02-11 13:52:35 +0200

    commit

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -3,14 +3,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 
 const Burger = (props) =>{
-    //const tranformedIngredients = Object.keys(props.ingredients)
-   // .map()
+    const tranformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{return [...Array(props.ingredients[igKey])]
+    .map((_,i) =>{return <BurgerIngredient key={igKey+i} type={igKey}/>})   
+    })
 
     return(
         <div className={classes.Burger}>
            <BurgerIngredient type={'bread-top'}/>
-           <BurgerIngredient type="cheese"/>
-           <BurgerIngredient type="meat"/>
+           {tranformedIngredients}
            <BurgerIngredient type="bread-bottom"/>
 
         </div>
